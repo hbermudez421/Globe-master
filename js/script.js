1b96301356da50e0134adc037d70b94558dfb859
Harold Bermudez <hbermudez421@west-mec.org>
2017-09-19 14:01:21 -0700

    Made changes
    Added script file to the globe project

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/* * * * * * * * * * * * * * * * * *
+ *   Globe - Capstone project      *
+ *   Harold Bermudez               *
+ *   Date 9/19/17                  *
+ * * * * * * * * * * * * * * * * * */
+
+'use strict'
+
+function validateForm() {
+  alert("test");
+}
+
+
+function createEventListeners() {
+  var form = document.getElementById("form");
+  if (form.addEventListener) {
+    form.addEventListener("submit", validateForm, false);
+  } else if (form.attachEvent) {
+    form.attachEvent("onsubmit", validateForm);
+  }
+}
+
+if (window.addEventListener) {
+  window.addEventListener("load", setUpPage, false);
+} else if (window.attachEvent) {
+  window.attachEvent("onload", setUpPage);
+}
